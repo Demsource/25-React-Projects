1df3fdebaa9a5a0a27754c3009f561363e7b056b
Demsource <demolomsadze@gmail.com>
2024-12-27 19:52:17 +0400

    Adding accordion project

---
+++ src/components/accordion/Accordion.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import data from "./data";
+import "./styles.css";
+
+const Accordion = () => {
+  const [selected, setSelected] = useState();
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(id) {
+    setSelected(id === selected ? null : id);
+  }
+
+  function handleMultiSelection(id) {
+    let copyMultiple = [...multiple];
+    let idIndex = copyMultiple.indexOf(id);
+
+    if (idIndex === -1) {
+      copyMultiple.push(id);
+    } else {
+      copyMultiple.splice(idIndex, 1);
+    }
+
+    setMultiple(copyMultiple);
+    console.log(copyMultiple);
+  }
+
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        {!enableMultiSelection ? "Enable multiple selection" : "Enable single selection"}
+      </button>
+      <div className="accordion">
+        {data && data.length ? (
+          data.map((item) => (
+            <div className="item" key={item.id}>
+              <div
+                className="title"
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(item.id)
+                    : () => handleSingleSelection(item.id)
+                }
+              >
+                <h3>{item.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(item.id) !== -1 && (
+                    <div className="content">{item.answer}</div>
+                  )
+                : selected === item.id && (
+                    <div className="content">{item.answer}</div>
+                  )}
+            </div>
+          ))
+        ) : (
+          <div>No data</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;

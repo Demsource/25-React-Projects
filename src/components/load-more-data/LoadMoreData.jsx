2e8b8d8cde018b31650a6a2f74dd76a541bc18ea
Demsource <demolomsadze@gmail.com>
2024-12-30 19:49:16 +0400

    Minor improvements

--- src/components/load-more-data/LoadMoreData.jsx
+++ src/components/load-more-data/LoadMoreData.jsx
@@ -12,14 +12,12 @@ const LoadMoreData = () => {
     try {
       setLoading(true);
       const response = await fetch(
-        `https://dummyjson.com/products?limit=20&skip=${
-          count === 0 ? 0 : count * 20
-        }`
+        `https://dummyjson.com/products?limit=20&skip=${count * 20}`
       );
 
       const result = await response.json();
 
-      if (result && result.products?.length) {
+      if (result?.products?.length) {
         setProducts((prevData) => [...prevData, ...result.products]);
         setLoading(false);
       }
@@ -34,7 +32,7 @@ const LoadMoreData = () => {
   }, [count]);
 
   useEffect(() => {
-    if (products && products.length === 100) setDisableButton(true);
+    if (products?.length && products.length === 100) setDisableButton(true);
   }, [products]);
 
   if (loading) return <div className="loading">Loading Data...</div>;

9327f6656f5e58ac44cfce6f93befe2b820b346a
Demsource <demolomsadze@gmail.com>
2025-01-03 21:39:56 +0400

    Fix merge: Random Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
 import "./App.css";
 import Accordion from "./components/accordion/Accordion";
 import GithubProfileFinder from "./components/github-profile-finder/GithubProfileFinder";
+import RandomColor from "./conponents/random-color/RandomColor";
 
 function App() {
   return (
     <div className="App">
       <Accordion />
       <GithubProfileFinder />
-=======
-import { useState } from "react";
-import "./App.css";
-import RandomColor from "./conponents/random-color/RandomColor";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
       <RandomColor />
->>>>>>> random-color-generator
     </div>
   );
 }

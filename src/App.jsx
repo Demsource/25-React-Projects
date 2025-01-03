288d03fd49e14d8ec27051261ae8c967d977059d
Demsource <demolomsadze@gmail.com>
2025-01-03 22:09:46 +0400

    Fix Import: Star Rating

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Accordion from "./components/accordion/Accordion";
 import GithubProfileFinder from "./components/github-profile-finder/GithubProfileFinder";
 import RandomColor from "./components/random-color/RandomColor";
-import StarRating from "./star-rating/StarRating";
+import StarRating from "./components/star-rating/StarRating";
 import ImageSlider from "./components/image-slider/ImageSlider";
 
 function App() {

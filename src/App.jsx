51e4fc5cee8a4e735883d31513875e4d73e9aa9d
Demsource <demolomsadze@gmail.com>
2025-01-03 21:41:45 +0400

    Minor fix: Random Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Accordion from "./components/accordion/Accordion";
 import GithubProfileFinder from "./components/github-profile-finder/GithubProfileFinder";
-import RandomColor from "./conponents/random-color/RandomColor";
+import RandomColor from "./components/random-color/RandomColor";
 
 function App() {
   return (

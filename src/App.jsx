2a9fd2a3a174b43d0b728ecdee924bbbe587bfc0
Demsource <demolomsadze@gmail.com>
2025-01-04 21:49:54 +0400

    Adding project: Use Responsive Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import UseResponsiveTest from "./components/use-responsive-custom-hook/UseResponsiveTest";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <UseResponsiveTest />
+    </div>
+  );
 }
 
 export default App;

f4f71d55305a55b0609704b9a7612035ab615fd4
Demsource <demolomsadze@gmail.com>
2025-01-04 20:59:00 +0400

    Adding project: Use Onclick Outside Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import UseOnclickOutsideTest from "./components/useOnclickOutside/UseOnclickOutsideTest";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <UseOnclickOutsideTest />
+    </div>
+  );
 }
 
 export default App;
